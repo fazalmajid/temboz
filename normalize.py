efa2514a4c74e987f02c185755bc9025ab919efb
majid <majid>
2004-03-28 10:32:18 +0000

    handle Unicode use modified date if created date is not available

--- normalize.py
+++ normalize.py
@@ -1,7 +1,12 @@
-import time, re, feedparser
+import time, re, feedparser, codecs
 
 #date_fmt = '%a, %d %b %Y %H:%M:%S %Z'
 date_fmt = '%Y-%m-%d %H:%M:%S'
+
+try:
+  parse_date = feedparser._parse_date
+except AttributeError:
+  parse_date = feedparser.parse_date
 
 def normalize(item, f):
   # get rid of RDF lossage...
@@ -13,8 +18,8 @@ def normalize(item, f):
   # title
   if 'title' not in item:
     item['title'] = 'Untitled'
-  if type(item['title']) != str:
-    print 'ß' * 72
+  if type(item['title']) not in [str, unicode]:
+    print 'TITLE' * 15
     import code
     from sys import exit
     code.interact(local=locals())
@@ -22,8 +27,10 @@ def normalize(item, f):
   if 'link' not in item:
     print 'E' * 16, item
     item['link'] = f['channel']['link']
+  if type(item['link']) == unicode:
+    item['link'] = str(item['link'])
   if type(item['link']) != str:
-    print 'ø' * 72
+    print 'LINK' * 18
     import code
     from sys import exit
     code.interact(local=locals())
@@ -34,44 +41,21 @@ def normalize(item, f):
       item['creator'] = f['channel']['creator']
   # date
   if 'date' not in item:
-    # XXX this code should go away in final versions
-    # find possible alternative date fields
-    sop = [k for k in item.keys() if k not in
-           ['description', 'link', 'title', 'guid', 'content_encoded',
-            'links', 'creator', 'summary', 'id', 'content',
-            'category', 'categories', 'comments']]
-    if sop:
-      print '#' * 72
-      import code
-      from sys import exit
-      code.interact(local=locals())
-    # /XXX
-    # XXX use HTTP last-modified date here
-    item['date'] = time.strftime(date_fmt, time.gmtime())
+    if 'modified' in item:
+      date = parse_date(item['modified'])
+    else:
+      date = None
   else:
-    date = feedparser.parse_date(item['date'])
-    if not date:
-      print '$' * 16, item['date']
-      date = time.gmtime()
-      item['date'] = time.strftime(date_fmt, time.gmtime())
-    item['date'] = time.strftime(date_fmt, date)
+    date = parse_date(item['date'])
+  if not date:
+    # XXX use HTTP last-modified date here
+    date = time.gmtime()
+  item['date'] = time.strftime(date_fmt, date)
   # modified date
   if 'modified' not in item:
-    # XXX this code should go away in final versions
-    # find possible alternative date fields
-    sop = [k for k in item.keys() if k not in
-           ['date', 'description', 'link', 'title', 'guid', 'content_encoded',
-            'links', 'creator', 'summary', 'id', 'content',
-            'category', 'categories', 'comments']]
-    if sop:
-      print '$' * 72
-      import code
-      from sys import exit
-      code.interact(local=locals())
-    # /XXX
     item['modified'] = None
   else:
-    modified = feedparser.parse_date(item['modified'])
+    modified = parse_date(item['modified'])
     # add a fudge factor within modifications are not counted as such
     # 10 minutes here
     if not modified or abs(time.mktime(modified) - time.mktime(date)) < 600:
@@ -88,6 +72,7 @@ def normalize(item, f):
     sop = [k for k in item.keys() if k not in
            ['description', 'link', 'title', 'guid', 'content_encoded',
             'links', 'creator', 'summary', 'id', 'date',
+            'summary_detail', 'title_detail',
             'modified_parsed', 'date_parsed', 'modified']]
     if sop:
       print '@' * 72
@@ -97,4 +82,8 @@ def normalize(item, f):
     else:
       content = '<a href="' + item['link'] + '">' + item['title'] + '</a>'
   item['content'] = content
+  # map unicode
+  for key in ['title', 'link', 'date', 'modified', 'creator', 'content']:
+    if type(item.get(key)) == unicode:
+      item[key] = item[key].encode('ascii', 'xmlcharrefreplace')
   
