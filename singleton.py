0b702948471dc5268289b8de6b0ef6073677d789
majid <majid>
2004-08-03 23:45:29 +0000

    fixed simple conflict

--- singleton.py
+++ singleton.py
@@ -33,6 +33,101 @@ class PseudoDB:
     self.db.rollback(*args, **kwargs)
     
 db = PseudoDB()
+<<<<<<< singleton.py
+
+class StdDev:
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.n = 0
+    self.sx = 0
+    self.sxx = 0
+
+  def step(self, x):
+    x = float(x)
+    self.n += 1
+    self.sx += x
+    self.sxx += x * x
+
+  def finalize(self):
+    val = math.sqrt((self.n * self.sxx - self.sx * self.sx) / self.n / self.n)
+    self.reset()
+    return str(val)
+  
+class IAAvg:
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.last = None
+    self.n = 0
+    self.sx = 0
+
+  def step(self, x):
+    x = float(x)
+    if self.last == None:
+      self.last = x
+    else:
+      assert x > self.last
+      x, self.last = x - self.last, x
+      self.n += 1
+      self.sx += x
+
+  def finalize(self):
+    if self.n == 0:
+      return 0
+    else:
+      val = self.sx / self.n
+      self.reset()
+      return str(val)
+  
+class IAStdDev:
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.last = None
+    self.n = 0
+    self.sx = 0
+    self.sxx = 0
+
+  def step(self, x):
+    x = float(x)
+    if self.last == None:
+      self.last = x
+    else:
+      assert x > self.last
+      x, self.last = x - self.last, x
+      self.n += 1
+      self.sx += x
+      self.sxx += x * x
+
+  def finalize(self):
+    if self.n == 0:
+      return 0
+    else:
+      val = math.sqrt(
+        (self.n * self.sxx - self.sx * self.sx) / self.n / self.n)
+      self.reset()
+      return str(val)
+  
+if __name__ == '__main__':
+  db.db.create_aggregate('stddev', 1, StdDev)
+  db.db.create_aggregate('inter_arrival_avg', 1, IAAvg)
+  db.db.create_aggregate('inter_arrival_stddev', 1, IAStdDev)
+  c = db.db.cursor()
+  c.execute("""select feed_title,
+  inter_arrival_avg(julianday(item_created)),
+  inter_arrival_stddev(julianday(item_created))
+  from fm_feeds, fm_items where feed_uid=item_feed_uid
+  group by feed_title
+  order by item_created""")
+  import pprint
+  pprint.pprint(c.fetchall())
+  import code
+  code.interact(local=locals())
+=======
 
 class StdDev:
   def __init__(self):
@@ -127,3 +222,4 @@ if __name__ == '__main__':
   order by item_created""")
   import pprint
   pprint.pprint(c.fetchall())
+>>>>>>> 1.3
