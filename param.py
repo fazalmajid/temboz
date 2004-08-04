bef5402a9a01354741e5cf0508283380bb20e026
majid <majid>
2004-08-04 03:58:07 +0000

    improved documentation of parameters

--- param.py
+++ param.py
@@ -1,15 +1,31 @@
-debug = True
+########################################################################
+#
+# Parameter file for Temboz
+#
+########################################################################
+
+# TCP port to use for the web server
 port = 9999
-temboz_url = 'http://www.temboz.com/'
+
 # number of RSS feeds fetched in parallel 
 feed_concurrency = 10
+
 # Maximum number of articles shown
 overload_threshold = 200
-# refresh interval in seconds
+
+# feed polling interval in seconds
 refresh_interval = 3600
+
+# URL to use as the User-Agent when downloading feeds
+temboz_url = 'http://www.temboz.com/'
 # user agent shown when fetching the feeds
 user_agent = 'Temboz (%s)' % temboz_url
+
 # page unauthenticated users should see
 unauth_page = temboz_url
+
 # dictionary of login/password
 auth_dict = {'majid': 'sopo'}
+
+#debug = True
+debug = False
