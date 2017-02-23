361fa157f9f2a03981978f0db1285c9a675aafbb
Fazal Majid <fazal@majid.org>
2017-02-22 19:48:03 -0800

    first draft of a Python packaging setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='temboz',
+    author='Fazal Majid',
+    author_email='python@sentfrom.com',
+    version='2.0',
+    url='http://github.com/fazalmajid/temboz',
+    packages=['temboz'],
+    description='The Temboz RSS/Atom feed reader and aggregator.',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+)
