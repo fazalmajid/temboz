5b343b3f9558ea003fe00b95d256ec0ea766923e
Fazal Majid <fazal@majid.org>
2023-01-11 20:24:08 +0000

    fixed #126 search broke with capitalization

--- tembozapp/fts5.py
+++ tembozapp/fts5.py
@@ -19,36 +19,44 @@ def fts5_term(term):
   out = []
   expect = None
   pending = []
+  WS = 'WORDSEP'
+  #print('-'*72)
+  #print(f'Parsing {term}')
   for c in term:
+    #print(f'c={c} in_q={in_q} implicit_q={implicit_q} out={out} expect={expect} pending={pending}')
     if c == "'": # SQL injection guard
       if pending:
+        if not implicit_q:
+          out.append('"')
+          implicit_q = True
         out.extend(pending)
         pending = None
       out.append("''")
-    elif expect:
-      if c != expect:
-        assert not in_q
+      continue
+    if expect:
+      assert not in_q
+      if expect == WS and c not in ' \t\n("' or expect != WS and c != expect:
         out.append('"')
+        implicit_q = True
         out.extend(pending)
-        out.append('"')
-        out.append(c)
+        expect = None
+        pending = None
+      elif expect == WS:
+        out.extend(pending)
         expect = None
         pending = None
       else:
         pending.append(c)
-        expect = {'N': 'D', 'D': None, 'R': None, 'O': 'T', 'T': None}[expect]
-        if expect is None:
-          assert not in_q
-          out.extend(pending)
-          expect = None
-          pending = None
-    elif c == '"':
+        expect = {'N': 'D', 'D': WS, 'R': WS, 'O': 'T', 'T': WS}[expect]
+        continue
+    if c == '"':
       if implicit_q:
         implicit_q = False
+        in_q = True
       else:
         in_q = not in_q
         out.append(c)
-    elif not in_q and c in 'AON':
+    elif not in_q and not implicit_q and c in 'AON':
       expect = {'A': 'N', 'O': 'R', 'N': 'O'}[c]
       pending = [c]
     elif c in ' \t\n()':
@@ -60,6 +68,8 @@ def fts5_term(term):
     else:
       if in_q:
         out.append(c)
+      elif implicit_q:
+        out.append(c)
       else:
         in_q = True
         implicit_q = True
@@ -67,11 +77,12 @@ def fts5_term(term):
         out.append(c)
   if pending:
     out.extend(pending)
-  if in_q:
+  if in_q or implicit_q:
     out.append('"')
   return ''.join(out)
 
 if __name__ == '__main__':
+  fts5_term('foo AND bar')
   for input, expected in [
       ('foo', '"foo"'),
       ('foo bar', '"foo" "bar"'),
@@ -81,6 +92,14 @@ if __name__ == '__main__':
       ('(foo AND bar) OR baz', '("foo" AND "bar") OR "baz"'),
       ('foo AN bar', '"foo" "AN" "bar"'),
       ('"foo AN bar"', '"foo AN bar"'),
+      ('ACME', '"ACME"'),
+      ('Acme', '"Acme"'),
+      ('OpenBSD', '"OpenBSD"'),
+      ('ANAN', '"ANAN"'),
+      ('ANAND', '"ANAND"'),
+      ('AN"D"', '"AND"'),
+      ('NOTAM', '"NOTAM"'),
+      ('ANDROS', '"ANDROS"'),
   ]:
     out = fts5_term(input)
     assert out == expected, \
